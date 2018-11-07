8aa5921ebbb9b0f8b95b9b2e77a32ef8e18d07db
Voidbound <42214421+Voidbound@users.noreply.github.com>
2018-11-07 21:36:52 +0200

    Initial commit

---
+++ app/src/main/java/com/coding_quest/gr_an/tei_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.coding_quest.gr_an.tei_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
