70d60e00cffb5c984e2576d3747e779a9eaf95cc
Voidbound <dagkiller97@gmail.com>
2018-11-19 10:15:38 +0200

    * Added ArtActivity * Added First Button * Added Fonts * Added Images * Wrote Java code for Button onClick event

---
+++ app/src/main/java/com/coding_quest/gr_an/tei_project/ArtActivity.java
@@ -0,0 +1,13 @@
+package com.coding_quest.gr_an.tei_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art);
+    }
+}
--- app/src/main/java/com/coding_quest/gr_an/tei_project/MainActivity.java
+++ app/src/main/java/com/coding_quest/gr_an/tei_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.coding_quest.gr_an.tei_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.art_button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ArtActivity.class));
+            }
+
+        });
     }
 }
+
