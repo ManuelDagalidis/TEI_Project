2d4ce8ae046a48377c491aeb419cc038e7b62446
NiotisGia <44878756+niotisgia@users.noreply.github.com>
2018-12-30 22:51:12 +0200

    Added more artists

---
+++ app/src/main/java/com/coding_quest/gr_an/tei_project/RecyclerViewAdapter1.java
@@ -0,0 +1,25 @@
+package com.coding_quest.gr_an.tei_project;
+
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class RecyclerViewAdapter1 {
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        ImageView image;
+        TextView name;
+
+
+
+
+
+    }
+
+
+
+
+}
