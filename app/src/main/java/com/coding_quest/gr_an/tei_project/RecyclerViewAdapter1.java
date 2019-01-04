ca90e1cae7cb4e73b1ffe98bedccd374c9b651f0
NiotisGia <44878756+niotisgia@users.noreply.github.com>
2019-01-04 15:46:26 +0200

    Added more artists

--- app/src/main/java/com/coding_quest/gr_an/tei_project/RecyclerViewAdapter1.java
+++ app/src/main/java/com/coding_quest/gr_an/tei_project/RecyclerViewAdapter1.java
@@ -1,13 +1,36 @@
 package com.coding_quest.gr_an.tei_project;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class RecyclerViewAdapter1 {
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class  RecyclerViewAdapter1 extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+        private static final String TAG = "RecyclerViewAdapter1";
+
+        private ArrayList<String> mPedalNames = new ArrayList<>();
+        private ArrayList<String> mPedalImages = new ArrayList<>();
+        private Context mContext;
+
+
+        public RecyclerViewAdapter1(Context context, ArrayList<String> pedalNames, ArrayList<String> pedalImages) {
+
+            this.mPedalNames = pedalNames;
+            this.mPedalImages = pedalImages;
+            this.mContext = context;
+
+        }
+
+
+        @NonNull
+        @Override
+        public View
 
 
         ImageView image;
