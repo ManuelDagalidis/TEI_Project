ab43b2ba1494e1efdc23cd94dc0ea62acefea79e
NiotisGia <44878756+niotisgia@users.noreply.github.com>
2019-01-04 16:55:58 +0200

    Added more artists

--- app/src/main/java/com/coding_quest/gr_an/tei_project/RecyclerViewAdapter1.java
+++ app/src/main/java/com/coding_quest/gr_an/tei_project/RecyclerViewAdapter1.java
@@ -3,15 +3,19 @@ package com.coding_quest.gr_an.tei_project;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class RecyclerViewAdapter1 {
 
 
-    public class  RecyclerViewAdapter1 extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    public class RecyclerViewAdapter1 extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
         private static final String TAG = "RecyclerViewAdapter1";
 
         private ArrayList<String> mPedalNames = new ArrayList<>();
@@ -30,19 +34,63 @@ public class RecyclerViewAdapter1 {
 
         @NonNull
         @Override
-        public View
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_artist_spec, parent, false);
+            ViewHolder holder = new ViewHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+            Log.d(TAG, "onBindViewHolder: called.");
+
+            Glide.with(mContext)
+                    .asBitmap()
+                    .load(mPedalImages.get(position))
+                    .into(holder.pedalImage);
+
+            holder.pedalName.setText(mPedalNames.get(position));
+
+            holder.artistpedalLayout.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, "onClick: clicked on: " + mPedalNames.get(position));
+
+                    Intent intent = new Intent(mContext, Pedal.class);
+                    intent.putExtra("pedalImage_res", mPedalImages.get(position));
+                    intent.putExtra("pedalName", mPedalNames.get(position));
+                    mContext.startActivity(intent);
+                }
+            });
 
 
-        ImageView image;
-        TextView name;
+        }
 
 
-        public ViewHo
+        @Override
+        public int getItemCount() {
+            return mPedalImages.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            ImageView pedalImage;
+            TextView pedalName;
+            RelativeLayout artistpedalLayout;
+
+            public ViewHolder(@NonNull View itemView) {
+
+                super(itemView);
+                pedalImage = itemView.findViewById(R.id.pedal_image);
+                pedalName = itemView.findViewById(R.id.pedal_name);
+                artistpedalLayout = itemView.findViewById(R.id.artistspec_layout);
+            }
+
+        }
 
 
     }
-
-
 
 
 }
